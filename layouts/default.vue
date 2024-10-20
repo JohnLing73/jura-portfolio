6e517ff454df02279d873a72130c73391be78c50
JohnLing <dudulala73@gmail.com>
2024-10-20 16:32:04 +0800

    14

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- Layout 內容在此 -->
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <!-- Page 內容在 slot -->
+    <slot />
+  </div>    
+</template>
